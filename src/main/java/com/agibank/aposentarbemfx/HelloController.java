39f88ba022d867f4ba3654e7ae2853776fde87f7
Matheus Stefan <roccomatheus0@gmail.com>
2025-04-06 18:04:51 -0300

    adicionar javafx e tela inicial

--- src/main/java/com/agibank/aposentarbemfx/HelloController.java
+++ src/main/java/com/agibank/aposentarbemfx/HelloController.java
@@ -2,13 +2,28 @@ package com.agibank.aposentarbemfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
+    private ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        // Load the image
+        Image image = new Image("file:assets/aposentarBem.JPG");
+        imageView.setImage(image);
+
+    }
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Hello, World!");
     }
 }
